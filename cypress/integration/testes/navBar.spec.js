61b4cd2cb76444a59cc6e435f07418e1a376b83e
Filipe Conde Pereira <filipe.pereira@pampa.compasso>
2021-05-26 17:06:18 -0300

    cria teste da barra de navegação

---
+++ cypress/integration/testes/navBar.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+context('Navigarion Bar', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
+    it('deve acessar área "sobre mim" ao clicar no link', () => {
+        cy.get('.effect6 > :nth-child(1) > .b').click();
+        cy.url().should('contain', 'https://www.filipeconde.com.br/sobre-mim/');
+        cy.get('.sc-bxivhb').should('be.visible');
+        cy.get('h2').should('contain', 'Oi! Eu sou Filipe');
+    })
+
+    it('deve acessar área "serviços" ao clicar no link', () => {
+        cy.get('.effect6 > :nth-child(2) > .b').click();
+        cy.url().should('contain', 'https://www.filipeconde.com.br/servicos/');
+        cy.get('.strip-gallery-item').should('have.length', 3);
+    })
+
+    it.only('deve acessar área "channel" ao clicar no link', () => {
+        cy.get('.effect6 > :nth-child(3) > .b').click();
+        cy.url().should('contain', 'https://www.filipeconde.com.br/channel/');
+        cy.get('.feed-layout-one-section > :nth-child(1)').should('be.visible');
+        cy.get('.feed-layout-one-section > :nth-child(2)').scrollIntoView().should('be.visible');
+        cy.get(':nth-child(3) > :nth-child(1) > .medium-article-cover').should('be.visible');
+    })
+
+    it('deve acessar área "contato" ao clicar no link', () => {
+        cy.get('.effect6 > :nth-child(4) > .b').click();
+        cy.url().should('contain', 'https://www.filipeconde.com.br/contato/');
+
+    })
+})

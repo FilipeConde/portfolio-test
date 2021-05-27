ad7a9475efed0b2e09d8457faee15f405d833e40
Filipe Conde Pereira <filipe.pereira@pampa.compasso>
2021-05-27 14:21:01 -0300

    cria teste para as galerias 'estúdio', 'exposição' e 'evento'

--- cypress/integration/testes/servicos.spec.js
+++ cypress/integration/testes/servicos.spec.js
@@ -11,6 +11,7 @@ context('Página "Serviços"', () => {
 
     describe('a primeira deve acessar galeria "Estúdio"', () => {
         it('acessa galeria a galeria', () => {
+            cy.visit('/servicos');
             cy.get('.strip-gallery-item:nth-of-type(1)').click();
         })
         it('deve exibir título "Estúdio" em destaque', () => {
@@ -24,6 +25,46 @@ context('Página "Serviços"', () => {
         it('deve exibir 13 imagens', () => {
             cy.scrollTo('bottom').get('.sc-bxivhb').should('be.visible').and('have.length', 13);
         })
+        //elaborar teste para navegação de imagem na galeria
     })
-    //elaborar teste para navegação de imagem na galeria
+
+    describe('a segunda deve acessar galeria "Exposição"', () => {
+        it('acessa galeria a galeria', () => {
+            cy.visit('/servicos');
+            cy.get('.strip-gallery-item:nth-of-type(2)').click();
+        })
+        it('deve exibir título "Estúdio" em destaque', () => {
+            cy.get('.border').should('be.visible').and('have.text', 'Exposição');
+        })
+
+        it('deve exibir galeria de imagens', () => {
+            cy.get('.thing-grid').should('be.visible');
+        })
+
+        it('deve exibir 13 imagens', () => {
+            cy.scrollTo('bottom').get('.sc-bxivhb').should('be.visible').and('have.length', 17);
+        })
+        //elaborar teste para navegação de imagem na galeria
+    })
+
+    describe('a terceira deve acessar galeria "Evento"', () => {
+        it('acessa galeria a galeria', () => {
+            cy.visit('/servicos');
+            cy.get('.strip-gallery-item:nth-of-type(3)').click();
+        })
+        it('deve exibir título "Estúdio" em destaque', () => {
+            cy.get('.border').should('be.visible').and('have.text', 'Evento');
+        })
+
+        it('deve exibir galeria de imagens', () => {
+            cy.get('.thing-grid').should('be.visible');
+        })
+
+        it('deve exibir 13 imagens', () => {
+            cy.scrollTo('bottom').get('.sc-bxivhb').should('be.visible').and('have.length', 7);
+        })
+        //elaborar teste para navegação de imagem na galeria
+    })
+
+
 })

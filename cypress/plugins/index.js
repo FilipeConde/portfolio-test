4ae05ad6138d4348a7b91e4c22110f5915d3e789
Filipe Conde <filipecondep@gmail.com>
2021-05-31 10:27:26 -0300

    finaliza configurações do cucumber e cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,20 +1,20 @@
 /// <reference types="cypress" />
-
-const cucumber = require('cypress-cucumber-preprocessor').default;
-const fs = require('fs-extra');
-const path = require('path');
-
-function getConfigurationFile(file) {
-  const pathToConfigFile = path.resolve('.', 'cypress', 'config', `${file}.json`);
-  return fs.readJson(pathToConfigFile);
+ 
+const cucumber = require('cypress-cucumber-preprocessor').default
+const fs = require('fs-extra')
+const path = require('path')
+ 
+function getConfigurationByFile(file){
+  const pathToConfigFile = path.resolve('.', 'cypress', 'config', `${file}.json`)
+  return fs.readJson(pathToConfigFile)
 }
-
+ 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  on("file:processor", cucumber());
+  on('file:preprocessor', cucumber())
   on('before:browser:launch', (browser = {}, launchOptions) => {
     if (browser.family === 'chromium' && browser.name !== 'electron') {
       launchOptions.args.push('--disable-dev-shm-usage')
